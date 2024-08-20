67f61d0526b4e585e3d468ffe34fc93733c9a333
Patrick Tran <patrick.tran06@gmail.com>
2024-08-20 16:41:12 -0700

    delete console log

--- main.js
+++ main.js
@@ -22,8 +22,6 @@ async function displayRecentUrls() {
     )
     .join("");
 
-  console.log(urlList);
-
   document.querySelector("#app").innerHTML = `
     <h1>Hello Vite!</h1>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
